dd62f7e7be9b904ac7045a966387f24c815a999b
Sandeep Kushwaha <shrisandeepkushwaha@gmail.com>
2024-06-17 21:42:25 +0530

    First Route and View

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    //return "About Page";
+    return view('about');
+});
+
