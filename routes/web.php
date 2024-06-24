ca536e16c26c16dd820f52012ec93f7149157971
Sandeep Kushwaha <160579425+shrisandeepkushwaha@users.noreply.github.com>
2024-06-25 01:09:00 +0530

    Autoloading, Namespaces, and Models

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Arr;
+use App\Models\Job;
 
 Route::get('/', function () {
     /*return view('home',[
@@ -36,54 +37,58 @@ Route::get('/contact', function () {
     return view('contact');
 });
 
+// $jobs = [
+//         [
+//             'id' => 1,
+//             'title' => 'Director',
+//             'salary' => '$50,000'
+//         ],
+//         [
+//             'id' => 2,
+//             'title' => 'Programmer',
+//             'salary' => '$10,000'
+//         ],
+//         [
+//             'id' => 3,
+//             'title' => 'Teacher',
+//             'salary' => '$40,000'
+//         ],
+// ];
+
+// class Job {
+//     public static function all() {
+//         return [
+//             [
+//                 'id' => 1,
+//                 'title' => 'Director',
+//                 'salary' => '$50,000'
+//             ],
+//             [
+//                 'id' => 2,
+//                 'title' => 'Programmer',
+//                 'salary' => '$10,000'
+//             ],
+//             [
+//                 'id' => 3,
+//                 'title' => 'Teacher',
+//                 'salary' => '$40,000'
+//             ],
+//         ];
+//     }
+// }
+
 Route::get('/jobs', function () {
-    return view('jobs',[
-        'jobs' => 
-        [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40,000'
-            ]
-        ]
-    ]);
+    return view('jobs',['jobs'=> Job::all()]);
 });
 
-Route::get('/job/{id}', function($id){
-    $jobs = [
-        [
-            'id' => 1,
-            'title' => 'Director',
-            'salary' => '$50,000'
-        ],
-        [
-            'id' => 2,
-            'title' => 'Programmer',
-            'salary' => '$10,000'
-        ],
-        [
-            'id' => 3,
-            'title' => 'Teacher',
-            'salary' => '$40,000'
-        ]
-
-    ];
-
+Route::get('/job/{id}', function($id) {
+    
     // $job = Arr::first($jobs, function($job) use ($id){
     //     return $job['id'] === $id;
     // });
 
-    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+    //$job = Arr::first(Job::all(), fn($job) => $job['id'] == $id);
+    $job = Job::find($id);
 
     return view('job',['job' => $job]);
 });
